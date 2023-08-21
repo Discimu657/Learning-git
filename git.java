65bdc2495f6b408233fd169a07e282299dad33b2
Discimu657 <discimu657@gmail.com>
2023-08-21 21:47:28 +0530

    Branch created now

--- git.java
+++ git.java
@@ -6,6 +6,6 @@ public class git{
 
     public static void main(String[]args){
 
-        System.out.println("Hello git branch 1");
+        System.out.println("Hello git branch created now!");
     }
 }
