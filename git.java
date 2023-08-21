0256db5312e3b54ea3257ac90427dce7c8693655
Discimu657 <142803519+Discimu657@users.noreply.github.com>
2023-08-21 22:31:24 +0530

    Learning pull.git

--- git.java
+++ git.java
@@ -9,7 +9,8 @@ public class git{
 
         System.out.println("Hello git branch 1");
         System.out.println("Line 2 merge check");
-
+System.out.pritnln("Checking pull 2");
+    
         System.out.println("Hello git branch created now!");
         System.out.println("Checkin out pulllll");
 
