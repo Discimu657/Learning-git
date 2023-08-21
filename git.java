c03af76eb56e977302ec14670d7e044892906f7e
Discimu657 <discimu657@gmail.com>
2023-08-21 22:33:46 +0530

    Learning merge.git

--- git.java
+++ git.java
@@ -9,7 +9,7 @@ public class git{
 
         System.out.println("Hello git branch 1");
         System.out.println("Line 2 merge check");
-System.out.pritnln("Checking pull 2");
+System.out.println("Checking pull 2");
     
         System.out.println("Hello git branch created now!");
         System.out.println("Checkin out pullllllllllllqert");
